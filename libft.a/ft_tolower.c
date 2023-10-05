84e0770a26f2774d58c598404f8291f1a3aacb19
jbidaux <jeremie.bidaux@gmail.com>
2023-10-05 11:44:46 +0200

    tolower and toupper

--- libft.a/ft_tolower.c
+++ libft.a/ft_tolower.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int ft_toupper(int c)
+int ft_tolower(int c)
 {
     if((c < 65 || c > 90) && (c < 97 || c > 122))
         return (c);
     if(c > 64 && c < 90)
-        return (c);
+        return (c + 32);
     if(c > 96 && c < 123)
-        return (c - 32);
+        return (c);
 }
 /* 
 int main(int ac, char **av)
@@ -19,7 +19,7 @@ int main(int ac, char **av)
     int src;
     src = av[1][0];
 
-    printf("Result: %d\n", ft_toupper(src));
-    printf("Result: %d\n", toupper(src)); 
+    printf("Result: %d\n", ft_tolower(src));
+    printf("Result: %d\n", tolower(src)); 
     return(0);
 } */
