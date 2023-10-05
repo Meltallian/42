f96b40f60231b52eb6ef3a021becf3382c86b10a
jbidaux <jeremie.bidaux@gmail.com>
2023-10-05 15:28:41 +0200

    memcmp done

---
+++ libft.a/ft_memcmp.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    size_t i;
+    const unsigned char *src1;
+    const unsigned char *src2;
+    i = 0;
+    src1 = s1;
+    src2 = s2;
+
+    while(i < n)
+    {
+        if(src1[i] != src2[i])
+            return(src1[i] - src2[i]);
+        i++;
+    }
+    return(0);
+}
+/* 
+int main(int ac, char **av)
+{
+    if(ac != 2)
+        return(1);
+
+    char *check;
+    check = av[1];
+
+    printf("Result: %d\n", ft_strlen(check));
+    printf("Result: %ld\n", strlen(check));
+    return(0);
+} */
