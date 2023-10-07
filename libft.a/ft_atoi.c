99d02f42f9f5c71f83e9032aa50e9bea21f067bf
jbidaux <jeremie.bidaux@gmail.com>
2023-10-07 15:13:18 +0200

    atoi done

---
+++ libft.a/ft_atoi.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_atoi(const char *str)
+{
+    long int n;
+    int i;
+    int neg;
+    neg = 1;
+    i = 0;
+    n = 0;
+
+    while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+        i++;
+    while(str[i] == 43 || str[i] == 45)
+    {
+        if(str[i] == 45)
+            neg = neg * (-1);
+        i++;
+    }
+    while(str[i] >= 48 && str[i] <= 57)
+    {
+        n = (n + (str[i] - 48))*10;
+        i++;
+    }
+    return(neg*(n/10));
+}
+
+/* int main(int ac, char **av)
+{
+    if(ac != 2)
+        return(1);
+
+    char *str;
+    str = av[1];
+
+    printf("Result: %d\n", ft_atoi(str));
+    return(0);
+} */
