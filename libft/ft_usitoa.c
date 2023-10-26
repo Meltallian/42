18b46b7b0c18b6e69bd8c562584e25fe623768bd
Jeremie Bidaux <jbidaux@c1r11s1.42lausanne.ch>
2023-10-26 13:28:37 +0200

    updated usitoa.c works tested

--- libft/ft_usitoa.c
+++ libft/ft_usitoa.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_usitoa.c                                          :+:      :+:    :+:   */
+/*   ft_usitoa.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 17:02:23 by jbidaux           #+#    #+#             */
-/*   Updated: 2023/10/13 17:58:27 by jbidaux          ###   ########.fr       */
+/*   Created: 2023/10/26 11:20:41 by jbidaux           #+#    #+#             */
+/*   Updated: 2023/10/26 11:28:46 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	size_count(int n)
+static int	size_count(unsigned int n)
 {
-	size_t	i;
+	int	i;
 
 	i = 1;
 	while (n > 9)
@@ -25,7 +25,7 @@ static size_t	size_count(int n)
 	return (i);
 }
 
-static char	*buff(char *str, int i, int n)
+static char	*buff(char *str, int i, unsigned int n)
 {
 	while (--i >= 0)
 	{
@@ -37,8 +37,8 @@ static char	*buff(char *str, int i, int n)
 
 char	*ft_usitoa(unsigned int n)
 {
-	char			*str;
-	unsigned int	i;
+	char	*str;
+	int		i;
 
 	i = size_count(n);
 	str = ft_calloc(i + 1, sizeof(char));
@@ -47,13 +47,3 @@ char	*ft_usitoa(unsigned int n)
 	str = buff(str, i, n);
 	return (str);
 }
-/*
-int	main(void)
-{
-	unsigned int	n;
-
-	n = 3147647;
-	printf("%s", ft_usitoa(n));
-	return (0);
-}
- */
