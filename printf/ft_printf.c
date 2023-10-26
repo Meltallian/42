1641335dfde8c7bfbda5fc1996843b8c899b9a2f
Jeremie Bidaux <jbidaux@c1r11s1.42lausanne.ch>
2023-10-26 13:55:49 +0200

    terminated, what a pain. and can't tell why

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,54 @@
 /*   By: jbidaux <jeremie.bidaux@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 09:51:35 by jbidaux           #+#    #+#             */
-/*   Updated: 2023/10/26 11:33:15 by jbidaux          ###   ########.fr       */
+/*   Updated: 2023/10/26 13:55:07 by jbidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+char	*ft_usitoa(unsigned int n)
+{
+	char			*str;
+	int				len;
+	unsigned int	tmp;
+
+	len = 1;
+	tmp = n;
+	while (tmp > 9)
+	{
+		tmp = tmp / 10;
+		len++;
+	}
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (0);
+	while (len > 0)
+	{
+		str[len - 1] = (n % 10) + 48;
+		n = n / 10;
+		len--;
+	}
+	return (str);
+}
+
+t_ull	ft_conv_u(unsigned int num)
+{
+	char			*str;
+	unsigned int	j;
+
+	j = 0;
+	str = ft_usitoa(num);
+	if (!str)
+		return (0);
+	while (str[j])
+	{
+		ft_putchar(str[j]);
+		j++;
+	}
+	free(str);
+	return (j);
+}
 
 t_ull	ft_format(va_list va, char *format, t_ull	*i)
 {
